b29d216108b59827667a985357602a15594415dc
Alan Garduño P <18728488+AlanGPmx@users.noreply.github.com>
2021-10-21 13:38:12 -0500

    Se agrega la lógica para guardar un registro de evento nuevo

--- Models/insert_user.php
+++ Models/insert_user.php
@@ -1,28 +1,45 @@
 <?php
 require_once "connection.php";
+date_default_timezone_set('America/Mexico_City');
+
+
 class EmpleadosM extends connection{
 
-    static public function RegistrarAsistenciaEM($datosf,$TBD){
+    static public function RegistrarAsistenciaEM($datosf,$TBD = "registros"){
 
-        date_default_timezone_set('America/Mexico_City');
         $fecha = date("Y-m-d");
         $hora = date("H:i:s");
-        
-        $pdo = connection::Conexion()->prepare("INSERT INTO $TBD (fecha_reg, hora_reg, id_usr, id_evento) VALUES 
-        (:fecha_reg, :hora_reg, :id_usr, :id_evento)");
 
-        $pdo->BindParam(":fecha_reg", $fecha, PDO::PARAM_STR);
-        $pdo->BindParam(":hora_reg", $hora, PDO::PARAM_STR);
-        $pdo->BindParam(":id_usr", $datosf["id_usr"], PDO::PARAM_STR);
-        $pdo->BindParam(":id_evento", $datosf["id_evento"], PDO::PARAM_STR);
+        //Obtener el ID del usuario (PK), no el Numero de empleado ni algún otro dato
+        $dataUser = connection::Conexion()->prepare("SELECT id FROM usuarios WHERE numEmpleado = :numEmpleado");
+        $dataUser->BindParam(":numEmpleado", $datosf["numEmpleado"], PDO::PARAM_STR);
+        $dataUser->execute();
+        $datosUsuario = $dataUser->fetch(PDO::FETCH_ASSOC);
 
-        if ($pdo->execute()) {
-            return "bien";
-        }else {
-            return "error";
+        if( $dataUser->rowCount() == 0 ){ //No existe el usuario
+            return 0;
         }
 
-        $pdo->Close();
+        // Verificar que el dia de la peticion no exista un registro de entrada, en caso de exisitir la peticion es de salida, si no será de entrada.
+        $verificarEntradaPrevia = connection::Conexion()->prepare("SELECT id FROM $TBD WHERE id_usuario = :id_usuario AND fecha = :today");
+        $verificarEntradaPrevia->BindParam(":id_usuario", $datosUsuario["id"], PDO::PARAM_STR);
+        $verificarEntradaPrevia->BindParam(":today", $fecha, PDO::PARAM_STR);
+        $existeEntradaPrevia = $verificarEntradaPrevia->execute();
+        $id_evento = ( $existeEntradaPrevia && $verificarEntradaPrevia->rowCount() != 0) ? 2:1; // 2 -> Salida | 1 -> Entrada
+
+        //Insertar el registro en la tabla
+        $pdo = connection::Conexion()->prepare("INSERT INTO $TBD (fecha, hora, id_usuario, id_evento) VALUES (:fecha, :hora, :id_usuario, :id_evento)");
+        $pdo->BindParam(":fecha", $fecha, PDO::PARAM_STR);
+        $pdo->BindParam(":hora", $hora, PDO::PARAM_STR);
+        $pdo->BindParam(":id_usuario", $datosUsuario["id"], PDO::PARAM_STR);
+        $pdo->BindParam(":id_evento", $id_evento, PDO::PARAM_STR);
+        $pdo->execute();
+
+        $r = $pdo->errorInfo();
+
+        $pdo = $verificarEntradaPrevia = $dataUser = null;
+        return $r;
+
     }   
 }
 ?>
