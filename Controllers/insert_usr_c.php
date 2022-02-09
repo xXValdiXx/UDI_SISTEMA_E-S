d8c2ec9f5f5b831af7a2271319c6d8ec237576ba
xXValdiXx <avaldivias1400@alumno.ipn.mx>
2022-02-09 17:51:17 -0600

    'Controlador para insertar nuevo usuario'

--- Controllers/insert_usr_c.php
+++ Controllers/insert_usr_c.php
@@ -15,6 +15,30 @@ class EmpleadosC{
         }
          
     }
+    static public function InsertarUDashC(){
+      
+        if(isset($_POST["nombre"])){
+            $datosUsuario["nombre"] = $_POST["nombre"];
+            $datosUsuario["apellidoPaterno"] = $_POST["apellidoPaterno"];
+            $datosUsuario["apellidoMaterno"] = $_POST["apellidoMaterno"];
+            $datosUsuario["area"] = $_POST["area"];
+            $datosUsuario["puesto"] = $_POST["puesto"];
+            $datosUsuario["numEmpleado"] = $_POST["numEmpleado"];
+            $datosUsuario ["correo"] = $_POST["correo"];
+            $datosUsuario["password"] = $_POST["password"];
+            $hash = password_hash($_POST["password"],PASSWORD_DEFAULT);
+            $datosUsuario ["password"] = $hash;
+            $datosUsuario["imagen"] = $_POST["imagen"];
+            $datosUsuario["telefonoPersonal"] = $_POST["telefonoPersonal"];
+            $datosUsuario["extensionIPN"] = $_POST["extensionIPN"];
+            
+            $respuesta = EmpleadosM::InsertarUDash($datosUsuario);
+            return $respuesta;
+        }else{
+            return "error";
+        }
+         
+    }
 
 }
 ?>
